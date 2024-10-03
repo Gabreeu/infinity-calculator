435f2fda16bcbc7aa6e5c3eb66de9b06ca673955
Gabriel de Souza Carvalho <167354546+Gabreeu@users.noreply.github.com>
2024-10-03 10:02:03 -0300

    Adicionado Seno e Cosseno e deixando o cod mais limpo

--- calculator.c
+++ calculator.c
@@ -1,32 +1,32 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdbool.h>
 
 double mult(double n1, double n2);
-double div(double n1, double n2);   //aqui estamos declarando as funçoes para abaixo mostrarmos ao compilador o que elas fazem, 
-double soma(double n1, double n2);  //isso ajuda a manter o codigo organizado e limpo
+double div(double n1, double n2);  
+double soma(double n1, double n2);  
 double sub(double n1, double n2);
 double imc(double n1, double n2);
 double pot(double n1, double n2, double r);
 double raiz(double n1, double n2, double r);
+double sen(double n1);
 
 
 int main(){
     char op, op2;
     double v1, v2, r;
     r=0;
-
-    printf("escreva a operacao:\n");
-    printf("+  adicao\n");
-    printf("-  subtacao\n");
-    printf("*  multiplicacao\n");
-    printf("/  divisao\n");
-    printf("o  outros\n");
+   
+    printf("------------------------------------\n");
+    printf("escreva a operacao:\n+  adicao\n-  subtracao\n*  multiplicacao\n/  divisao\no  outros\n");
     scanf(" %c", &op);
+   //////////////
     if (op=='o'){
-      printf("I  Indice de massa corporea\n");
-      printf("P  Potenciacao de um numero\n");
-      printf("R  Raiz quadrada de um numero\n");
-      scanf(" %c",&op2); 
+      system("cls");
+      printf("------------------------------------\n");
+      printf("I  Indice de massa corporea\nP  Potenciacao de um numero\nR  Raiz quadrada de um numero\nS Seno\nC Cosseno\n");
+      scanf(" %c",&op2);
+      printf("------------------------------------\n"); 
       switch (op2) {
             case 'I':
                 printf("Digite o peso: ");
@@ -45,7 +45,15 @@ int main(){
                 scanf(" %lf",&v1);
                 printf("digite a potencia da raiz: ");
                 scanf(" %lf",&v2);
+                break;
             //se for adicionar mais operações adicione aqui
+            case 'S':
+                printf("digite o valor: ");
+                scanf(" %lf",&v1);
+                break;
+            case 'C':
+                printf("digite o valor: ");
+                scanf(" %lf",&v1);
                 break;
              default:
                 printf("Opcao invalida para 'outros'.\n");
@@ -53,10 +61,11 @@ int main(){
       }  
     }else{
         printf("Digite o valor 1: ");
-	    scanf("%lf",&v1);
+	     scanf("%lf",&v1);
 	    printf("Digite o valor 2: ");          
 	    scanf("%lf",&v2);
     }
+    ///////////processamento e saida
     switch (op){
         case '+':
             printf("Soma= %.2f", soma(v1,v2));
@@ -70,6 +79,7 @@ int main(){
         case '/':
             printf("Div= %.2f", div(v1,v2));
             break;
+        ///////////
         case 'o':
             switch (op2){
                 case 'I':
@@ -79,19 +89,24 @@ int main(){
                     printf("%.2f na potencia %.2f: %.2f", v1,v2,pot(v1,v2,r));
                     break;
                 case 'R':
-                    printf("raiz na potencia de %.2f de %2.f: %.2f", v1,v2,pot(v2,v1,r));
+                    printf("raiz na potencia de %.2f de %2.f: %.2f", v1,v2,raiz(v2,v1,r));
+                    break;
+                case 'C':
+                     printf("o cosseno de %2.f graus e: %f",v1,cos(v1));
+                    break;
+                case 'S':
+                    printf("o seno de %2.f graus e: %f",v1,sen(v1));
                     break;
                 default:
                     printf("valor invalido.\n");
                     break;
-
             }
         break;//impede que o case(op) exiba a mensagem de valor invalido quando nao era p aparecer
         default:
             printf("valor invalido.\n");
         break;
     }
-
+ 
     return 0;
 }
 
@@ -102,3 +117,5 @@ double sub(double n1, double n2){return n1-n2;}
 double imc(double n1, double n2){return n1/(n2*n2);}
 double pot(double n1, double n2, double r){r=pow(n1,n2); return r;}
 double raiz(double n1, double n2, double r){r=pow(n1,1.0/n2); return r;}
+double sen(double n1){return sin(n1);}
+double cos(double n1){return cos(n1);}
